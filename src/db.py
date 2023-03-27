27c9844526f2f1b9d66415c789261d460123d182
etozhefifan <coofi2012@gmail.com>
2023-03-27 14:00:30 +0300

    [Fix] black

--- src/db.py
+++ src/db.py
@@ -11,5 +11,3 @@ async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession
 async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
-
-
